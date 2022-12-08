91a4fc763a042de46811f21db56dcf6431304d2a
Daniel Novoa <danielnovoa@tuabi.mx>
2022-12-07 18:22:15 -0600

    prueba 6

--- abstraccion_clases.py
+++ abstraccion_clases.py
@@ -5,12 +5,10 @@ def run():
 
     lavadora = Lavadora().lavar()
 
-<<<<<<<<< Temporary merge branch 1
 
-=========
 print("hola")
 print("este cmabio es para probar el git develop")
->>>>>>>>> Temporary merge branch 2
+
 if __name__ == '__main__':
     run()
 
