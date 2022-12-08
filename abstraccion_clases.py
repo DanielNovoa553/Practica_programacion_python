8ef01a1229f6f6c73ff4659764e21e22afe054b6
Daniel Novoa <danielnovoa@tuabi.mx>
2022-12-07 18:23:02 -0600

    prueba 7

--- abstraccion_clases.py
+++ abstraccion_clases.py
@@ -6,7 +6,7 @@ def run():
     lavadora = Lavadora().lavar()
 
 
-print("hola")
+print("hola este cambio viene de curso poo")
 print("este cmabio es para probar el git develop")
 
 if __name__ == '__main__':
