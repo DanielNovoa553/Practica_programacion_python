1ff4cab53877ce47b928f6b98488d56194afb137
Daniel Novoa <danielnovoa@tuabi.mx>
2023-02-23 18:38:02 -0600

    excelformat

--- covid_api.py
+++ covid_api.py
@@ -1,13 +1,15 @@
 import os
 import tkinter as tk
+import pandas as pd
 import requests
 import json
 from datetime import datetime
-import pandas as pd
+
 
 def get_covid_data():
     # Hacer la solicitud HTTP GET a la API
     response = requests.get("https://disease.sh/v3/covid-19/all")
+
     # Verificar que la respuesta sea exitosa
     if response.status_code == 200:
         # Analizar el contenido JSON de la respuesta
@@ -25,6 +27,19 @@ def get_covid_data():
         critical = data["critical"]
         population = data["population"]
         affected_countries = data["affectedCountries"]
+
+        # sustituir el punto por coma en los numeros
+        total_cases = "{:,}".format(total_cases)
+        total_deaths = "{:,}".format(total_deaths)
+        today_cases = "{:,}".format(today_cases)
+        today_deaths = "{:,}".format(today_deaths)
+        recovered = "{:,}".format(recovered)
+        today_recovered = "{:,}".format(today_recovered)
+        active = "{:,}".format(active)
+        critical = "{:,}".format(critical)
+        population = "{:,}".format(population)
+        affected_countries = "{:,}".format(affected_countries)
+
         root = tk.Tk()
         root.title("Covid-19 Report")
         root.geometry("600x450")
@@ -44,7 +59,6 @@ def get_covid_data():
         label3 = tk.Label(root, font=f3, text="Reporte Covid 19", fg="grey")
         label3.config(text="Fecha: " + str(date))
         label3.pack(side="bottom", pady=15)
-
         root.mainloop()
 
         print("Información COVID-19")
@@ -59,16 +73,31 @@ def get_covid_data():
         print(f"Población: {population}")
         print(f"Países Afectados: {affected_countries}")
 
-        # df = pd.DataFrame(data, index=[0])
-        # df.drop(["updated", "tests", "testsPerOneMillion", "oneCasePerPeople", "oneDeathPerPeople", "oneTestPerPeople",
-        #          "activePerOneMillion", "recoveredPerOneMillion", "criticalPerOneMillion", "casesPerOneMillion",
-        #          "deathsPerOneMillion" ], axis=1, inplace=True)
-        # print(df)
-        # writer = pd.ExcelWriter("covid19.xlsx")
-        # df.to_excel( "covid19.xlsx", index=False, header=True, freeze_panes=(1, 0), engine='xlsxwriter', sheet_name="covid19")
-        # print("Datos exportados exitosamente a --> covid19.xlsx")
-        # filename = os.path.abspath("covid19.xlsx")
-        # os.startfile(filename)
+        df = pd.DataFrame(data, index=[0])
+        df.drop(["updated", "tests", "testsPerOneMillion", "oneCasePerPeople", "oneDeathPerPeople", "oneTestPerPeople",
+                 "activePerOneMillion", "recoveredPerOneMillion", "criticalPerOneMillion", "casesPerOneMillion",
+                 "deathsPerOneMillion" ], axis=1, inplace=True)
+        print(df)
+        writer = pd.ExcelWriter('covid19.xlsx', engine='xlsxwriter' )
+        df.to_excel(writer, sheet_name='Hoja1', index=False, header=True )
+        workbook = writer.book
+        worksheet = writer.sheets['Hoja1']
+        format1 = workbook.add_format({'num_format': '#,##0'})
+        worksheet.set_column('A:A', 15, format1)
+        worksheet.set_column('B:B', 15, format1)
+        worksheet.set_column('C:C', 15, format1)
+        worksheet.set_column('D:D', 15, format1)
+        worksheet.set_column('E:E', 15, format1)
+        worksheet.set_column('F:F', 15, format1)
+        worksheet.set_column('G:G', 15, format1)
+        worksheet.set_column('H:H', 15, format1)
+        worksheet.set_column('I:I', 15, format1)
+        worksheet.set_column('J:J', 15, format1)
+        writer.save()
+
+        print("Datos exportados exitosamente a --> covid19.xlsx")
+        filename = os.path.abspath("covid19.xlsx")
+        os.startfile(filename)
 
     else:
         # Manejar el error
