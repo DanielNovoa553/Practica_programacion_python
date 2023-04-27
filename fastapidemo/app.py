98aa0d6902af63288d5f978215af4ef286d58d98
Daniel Novoa <danielnovoa@tuabi.mx>
2023-04-26 19:39:11 -0600

    fastapi2

--- fastapidemo/app.py
+++ fastapidemo/app.py
@@ -1,23 +1,65 @@
+import json
+
+import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
-from fastapidemo.conexion import connectdb
+from conexion import connectdb
 
 app = FastAPI()
 
 class Libro(BaseModel):
-    titulo: str
-    autor: str
-    descripcion: str
+    nombre_libro: str
+    autor_libro: str
+    editorial_libro: str
+
+@app.get("/")
+def root():
+    return {"mensaje": "Hola mundo"}
 
 @app.post("/libros/")
 def crear_libro(libro: Libro):
 
     conexion = connectdb()
-    cursor = conexion.cursor()
-    consulta = "INSERT INTO libros (titulo, autor, descripcion) VALUES (%s, %s, %s)"
-    valores = (libro.titulo, libro.autor, libro.descripcion)
-    cursor.execute(consulta, valores)
-    conexion.commit()
-    conexion.close()
-    return {"mensaje": "Libro creado correctamente"}
+    if not conexion:
+        return {"mensaje": "Error al conectar a la base de datos"}
 
+    cursor = conexion.cursor()
+    try:
+        query = f"INSERT INTO libro (nombre_libro, autor_libro, editorial_libro) VALUES ('{libro.nombre_libro}', " \
+                f"'{libro.autor_libro}', '{libro.editorial_libro}')"
+
+        cursor.execute(query)
+        id_libro = cursor.lastrowid
+        conexion.commit()
+        nombre_libro = libro.nombre_libro
+        conexion.close()
+        return f"Libro {nombre_libro} insertado correctamente con id {id_libro}"
+
+    except Exception as e:
+        print("Ocurrió un error al insertar, detalle del error: ", e)
+        return {"mensaje": "Error al insertar el libro"}
+
+#obtener los libros llave valor en formato json
+@app.get("/get_libros/")
+def obtener_libros():
+    conexion = connectdb()
+    if not conexion:
+        return {"mensaje": "Error al conectar a la base de datos"}
+
+    cursor = conexion.cursor()
+    try:
+        query = f"SELECT * FROM libro"
+        cursor.execute(query)
+        libros = cursor.fetchall()
+        data = []
+        for libro in libros:
+            item ={}
+            for i in range(len(libro)):
+                item[cursor.description[i][0]] = libro[i]
+            data.append(item)
+        conexion.close()
+        return {"libros": data}
+
+    except Exception as e:
+        print("Ocurrió un error al obtener los libros, detalle del error: ", e)
+        return {"mensaje": "Error al obtener los libros"}
